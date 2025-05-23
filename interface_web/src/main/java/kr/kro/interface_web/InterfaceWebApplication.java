782f0b4c0dc3a885ef79bb5ec710e44644dd498b
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2025-05-23 21:11:49 +0900

    Initial commit

---
+++ interface_web/src/main/java/kr/kro/interface_web/InterfaceWebApplication.java
@@ -0,0 +1,13 @@
+package kr.kro.interface_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterfaceWebApplication.class, args);
+    }
+
+}
---
+++ interface_web/src/test/java/kr/kro/interface_web/InterfaceWebApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kro.interface_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfaceWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
